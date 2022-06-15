445d28ca77f54ebb95caa78a4227823a58b6b0f1
YakelinBarcoQuiroga <yakelinbarcoquiroga@gmail.com>
2022-06-15 18:14:21 -0500

    eliminación

--- Html/js/custom.js
+++
@@ -1,8 +0,0 @@
-const toggleMenuElement = document.getElementById('carshopping');
-const mainMenuElement = document.getElementById('hide');
-
-toggleMenuElement.addEventListener('click', ()=>{
-
-  mainMenuElement.classList.toggle('show');
-  
-}); 
